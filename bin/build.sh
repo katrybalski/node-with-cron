4b98c9897a2bd2687acbe5afc42961ef540211a6
Karol Trybalski <tkarol.04@gmail.com>
2021-04-24 17:13:04 +0200

    base image simplified

--- bin/build.sh
+++ bin/build.sh
@@ -4,10 +4,13 @@ set -e
 
 DIR=$( dirname $0 )
 
-ARM_VERSION="arm64v8/node:15.14.0-buster"
-IMAGE_NAME="node-with-cron:arm64v8"
+if [[ ${ARM} = true ]]; then
+    . ${DIR}/../.env-arm
+else
+    . ${DIR}/../.env
+fi
 
 docker build -t ${IMAGE_NAME} \
         -f ${DIR}/../docker/Dockerfile \
-        --build-arg BASE_IMAGE=${ARM_VERSION} \
+        --build-arg BASE_IMAGE=${BASE_IMAGE} \
         ${DIR}/..
