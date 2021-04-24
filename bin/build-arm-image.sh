ff23ddcf198a03b8fd7e8ebdd5aa77aab1247444
Karol Trybalski <tkarol.04@gmail.com>
2021-04-24 16:15:49 +0200

    added build arg

---
+++ bin/build-arm-image.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+DIR=$( dirname $0 )
+
+ARM_VERSION="arm64v8/node:15.14.0-buster"
+IMAGE_NAME="node-with-cron:arm64v8"
+
+docker build -t ${IMAGE_NAME} \
+        -f ${DIR}../docker/Dockerfile \
+        --build-arg BASE_IMAGE=${ARM_VERSION}
+        ${DIR}/..
