032e62b9900d6813e42bb3a0d73b91f2f54013c3
Karol Trybalski <tkarol.04@gmail.com>
2021-04-24 16:20:06 +0200

    Dockerfile fixes

--- bin/build-arm-image.sh
+++ bin/build-arm-image.sh
@@ -8,6 +8,6 @@ ARM_VERSION="arm64v8/node:15.14.0-buster"
 IMAGE_NAME="node-with-cron:arm64v8"
 
 docker build -t ${IMAGE_NAME} \
-        -f ${DIR}../docker/Dockerfile \
-        --build-arg BASE_IMAGE=${ARM_VERSION}
+        -f ${DIR}/../docker/Dockerfile \
+        --build-arg BASE_IMAGE=${ARM_VERSION} \
         ${DIR}/..
